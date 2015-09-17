6e7391bd5c8bf45646cbcf42ea28ebb8286f8c16
gregoryklein <gregoryhklein@gmail.com>
2015-09-17 13:30:57 -0700

    Added new sprites, scripts, and physics materials

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour 
+{
+	public bool player1 = true;
+
+	public float maxVelocityY;
+	public float verticalOffsetAngle;
+	public float thrustForce;
+
+	private Rigidbody2D rigidBody;
+
+	void Start()
+	{
+		rigidBody = GetComponent<Rigidbody2D>();
+	}
+
+	void FixedUpdate()
+	{
+		KeyCode rightMovementCode = player1 ? KeyCode.RightArrow : KeyCode.D;
+		KeyCode leftMovementCode = player1 ? KeyCode.LeftArrow : KeyCode.A;
+
+		if (Input.GetKey(leftMovementCode))
+		{
+			Vector3 direction = directionForKeyCode(KeyCode.LeftArrow);
+			applyForceWithDirection(direction);
+		}
+		else if(Input.GetKey (rightMovementCode))
+		{
+			Vector3 direction = directionForKeyCode(KeyCode.RightArrow);
+			applyForceWithDirection(direction);
+		}
+	}
+
+	private Vector3 directionForKeyCode(KeyCode code)
+	{
+		Vector3 direction = Vector2.zero;
+		switch (code)
+		{
+		case KeyCode.LeftArrow:
+			direction = Quaternion.AngleAxis(90 + verticalOffsetAngle, Vector3.forward) * Vector3.right;
+			break;
+		case KeyCode.RightArrow:
+			direction = Quaternion.AngleAxis(90 - verticalOffsetAngle, Vector3.forward) * Vector3.right;
+			break;
+		}
+		return direction;
+	}
+
+	private void applyForceWithDirection(Vector2 direction)
+	{
+		resetRigidBody();
+		rigidBody.AddForce(direction * thrustForce, ForceMode2D.Impulse);
+	}
+	
+	private void resetRigidBody()
+	{
+		rigidBody.isKinematic = true;
+		rigidBody.isKinematic = false;
+	}
+}
