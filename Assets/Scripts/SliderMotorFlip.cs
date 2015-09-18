def5ac12d8cf5cfd8585f299233320c94b345885
nicogrossfeld <nico@purevirtualstudios.com>
2015-09-17 19:35:49 -0700

    More nico updates

---
+++ Assets/Scripts/SliderMotorFlip.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SliderMotorFlip : MonoBehaviour
+{
+	private SliderJoint2D sliderJoint;
+	private float initialMotorSpeed;
+	
+	void Start()
+	{
+		sliderJoint = GetComponent<SliderJoint2D>();
+		initialMotorSpeed = sliderJoint.motor.motorSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update() 
+	{
+		updateSliderJointDirectionIfNecessary();
+	}
+	
+	private void updateSliderJointDirectionIfNecessary()
+	{	
+		bool motorDirectionShouldChange = false;
+		
+		// used to change dirction, pos:left, neg:right
+		float motorSpeedMultiplier = 1;
+		
+		switch (sliderJoint.limitState)
+		{
+		case JointLimitState2D.LowerLimit:
+		{
+			motorDirectionShouldChange = true;			
+			motorSpeedMultiplier = -1;
+			break;
+		}
+		case JointLimitState2D.UpperLimit:
+		{
+			motorDirectionShouldChange = true;
+			break;
+		}
+		default:
+			break;
+		}
+		
+		if (motorDirectionShouldChange)
+		{
+			JointMotor2D newMotor = new JointMotor2D();
+			newMotor.motorSpeed = initialMotorSpeed * motorSpeedMultiplier;
+			newMotor.maxMotorTorque = sliderJoint.motor.maxMotorTorque;
+			
+			sliderJoint.motor = newMotor;
+		}
+	}
+}
