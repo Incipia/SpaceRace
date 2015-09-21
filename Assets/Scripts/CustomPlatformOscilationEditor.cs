68f5bb22d0833784240cb41180d0c141805a3cbe
gregoryklein <gregoryhklein@gmail.com>
2015-09-20 18:31:58 -0700

    Started prototype on oscilation script

---
+++ Assets/Scripts/CustomPlatformOscilationEditor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+using System.Collections;
+using System.Collections.Generic;
+
+[CustomEditor(typeof(PlatformOscilation))]
+public class CustomPlatformOscilationEditor : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+		
+		PlatformOscilation myScript = (PlatformOscilation)target;
+		if (GUILayout.Button("Store Transform"))
+		{
+			myScript.storeTransform();
+		}
+		if (GUILayout.Button("Reset"))
+		{
+			myScript.reset();
+		}
+	}
+}
---
+++ Assets/Scripts/PlatformOscilation.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlatformOscilation : MonoBehaviour 
+{
+	public List<Vector3> positions;
+
+	public void reset()
+	{
+		positions.Clear();
+	}
+	
+	public void storeTransform()
+	{
+		positions.Add(transform.position);
+	}
+	
+	void OnDrawGizmos() 
+	{
+		Gizmos.color = Color.yellow;
+		
+		if (positions.Count >= 1)
+		{
+			Vector3 previousPosition = positions[0];
+			foreach (Vector3 position in positions)
+			{
+				Gizmos.DrawLine(previousPosition, position);
+				Gizmos.DrawSphere(previousPosition, .1f);
+				previousPosition = position;
+			}
+       	}
+   	}
+}
