9cd006ffce80ae4ce6f14b93326a9cd0b385bb2e
atesija <antesija@gmail.com>
2015-10-06 20:58:18 -0400

    Initial portal code

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Collider2D))]
+public class Portal : MonoBehaviour 
+{
+	public Collider2D exitPortal;
+
+	Collider2D entranceCollider;
+
+	// Use this for initialization
+	void Start () 
+	{
+		entranceCollider = GetComponent<Collider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		//float distance = Vector3.Distance(other.transform.position, transform.position);
+		other.gameObject.transform.position = exitPortal.transform.position;
+	}
+}
