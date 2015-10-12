337ae107f526837838c73a32ebb146f75fa67587
gregoryklein <gregoryhklein@gmail.com>
2015-10-12 19:13:02 -0400

    Using new player prefab and made MovePlayer and PlayerKeyboardInput inherit from MonoBehaviour

--- Assets/Scripts/PlayerKeyboardInput.cs
+++ Assets/Scripts/PlayerKeyboardInput.cs
@@ -2,26 +2,23 @@
 using System.Collections;
 using UnityEngine.Networking;
 
-public class PlayerKeyboardInput : NetworkBehaviour
+public class PlayerKeyboardInput : MonoBehaviour
 {
 	public bool player1 = true;
 	public MovePlayer movePlayer;
 
 	void Update()
 	{
-		if (isLocalPlayer)
+		KeyCode rightMovementCode = player1 ? KeyCode.RightArrow : KeyCode.Period;
+		KeyCode leftMovementCode = player1 ? KeyCode.LeftArrow : KeyCode.Comma;
+		
+		if (Input.GetKeyDown(leftMovementCode))
 		{
-			KeyCode rightMovementCode = player1 ? KeyCode.RightArrow : KeyCode.Period;
-			KeyCode leftMovementCode = player1 ? KeyCode.LeftArrow : KeyCode.Comma;
-			
-			if (Input.GetKeyDown(leftMovementCode))
-			{
-				movePlayer.jumpWithDirection(JumpDirection.Left);
-			}
-			if(Input.GetKeyDown(rightMovementCode))
-			{
-				movePlayer.jumpWithDirection(JumpDirection.Right);
-			}
+			movePlayer.jumpWithDirection(JumpDirection.Left);
+		}
+		if(Input.GetKeyDown(rightMovementCode))
+		{
+			movePlayer.jumpWithDirection(JumpDirection.Right);
 		}
 	}
 }
