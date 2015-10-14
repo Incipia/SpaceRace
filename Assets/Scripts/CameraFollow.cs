8de2c61dda105693fc8d5e57056e6f795d2d82ca
atesija <antesija@gmail.com>
2015-10-13 21:15:51 -0400

    New camera follow script

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour 
+{
+	public Transform objectToFollow;
+	public float followSpeed;
+	public bool lockXPosition = true;
+
+	Transform trans;
+
+	// Use this for initialization
+	void Start () {
+		trans = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 trackingPosition = objectToFollow.position;
+		trackingPosition.z = trans.position.z;
+
+		if(lockXPosition)
+		{
+			trackingPosition.x = trans.position.x;
+		}
+
+		trans.position = Vector3.Lerp(trans.position, trackingPosition, followSpeed);
+	}
+}
