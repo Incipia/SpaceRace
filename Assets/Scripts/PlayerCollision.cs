993c5a66fdc94374f90b1250bee03add8639b3c5
nicogrossfeld <nico@purevirtualstudios.com>
2015-11-04 20:28:50 -0800

    Added some seriously juice baby

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCollision : MonoBehaviour 
+{
+	public GameObject collisionFeedbackPrefab;
+	
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		Vector3 contactPosition = other.contacts[0].point;
+		GameObject collisionFeedbackGameObject = Instantiate(collisionFeedbackPrefab, contactPosition, Quaternion.identity) as GameObject;
+		PlayerCollisionFeedback collisionFeedback = collisionFeedbackGameObject.GetComponent<PlayerCollisionFeedback>();
+		if (collisionFeedback != null)
+		{
+			collisionFeedback.showFeedback();
+		}
+	}
+	
+//	void OnTriggerEnter2D(Collider2D other)
+//	{
+//		GameObject collisionFeedbackGameObject = Instantiate(collisionFeedbackPrefab, transform.position, Quaternion.identity) as GameObject;
+//		PlayerCollisionFeedback collisionFeedback = collisionFeedbackGameObject.GetComponent<PlayerCollisionFeedback>();
+//		if (collisionFeedback != null)
+//		{
+//			collisionFeedback.showFeedback();
+//		}
+//	}
+}
---
+++ Assets/Scripts/PlayerCollisionFeedback.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCollisionFeedback : MonoBehaviour 
+{
+	public ParticleSystem _particleSystem;
+	
+	public void showFeedback()
+	{
+		if (_particleSystem != null)
+		{
+			_particleSystem.Play();
+			Destroy(gameObject, _particleSystem.duration);
+		}
+	}
+}
