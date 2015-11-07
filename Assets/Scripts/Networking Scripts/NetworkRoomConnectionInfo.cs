502ad36b3b1a680cda78f9a4d11e40193dd2d1ec
gregoryklein <gregoryhklein@gmail.com>
2015-11-06 20:01:53 -0500

    More work on the initial screen

---
+++ Assets/Scripts/Networking Scripts/NetworkRoomConnectionInfo.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using AssemblyCSharp;
+using System.Collections;
+
+public class NetworkRoomConnectionInfo : MonoBehaviour 
+{
+	public CounterUI youArePlayerCounter;
+	public CounterUI openSpotsLeftCounter;
+
+	public void updatePlayerNumber(int number)
+	{
+		Color numberColor = PlayerColorProvider.colorForPlayerNumber(number, PlayerColoredComponentType.InnerCircle);
+		youArePlayerCounter.updateColor(numberColor);
+		youArePlayerCounter.updateSpritesWithNumber(number);
+	}
+
+	public void updateOpenSpotsLeft(int number)
+	{
+		openSpotsLeftCounter.updateSpritesWithNumber(number);
+	}
+}
